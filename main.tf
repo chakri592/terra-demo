58193c72f170ff9f52b4505532d93c8170d32b41
chakri592 <102147867+chakri592@users.noreply.github.com>
2024-10-18 21:40:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # EC2 Instance with Tomcat Installation
 resource "aws_instance" "two" {
-  ami                    = "ami-0d1622042e957c247"  # Replace with a valid Amazon Linux AMI ID
+  ami                    = "ami-0e0e417dfa2028266"  # Replace with a valid Amazon Linux AMI ID
   instance_type          = "t2.micro"
 
   tags = {
